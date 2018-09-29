fef5a303c3666da9da0d66b5f203d54ce939677d
lvliao <git@code.aliyun.com:te_lv02/test2.git>
2018-09-29 09:15:42 +0800

    卖家端微信登录(生成token放置到cookie中,在根据生成的token设置redis,键是token值是openid,分别设置cookie和redis的过期时间),登出销毁cookie

---
+++ src/main/java/com/imooc/constant/CookieConstant.java
@@ -0,0 +1,14 @@
+package com.imooc.constant;
+
+/**
+ * @program: sell
+ * @Date: 2018/9/28 22:33
+ * @Author: Mr.lv
+ * @Description: cookie 常量
+ */
+public interface CookieConstant {
+
+    String TOKEN = "token";
+
+    Integer EXPIRE = 7200;
+}
---
+++ src/main/java/com/imooc/constant/RedisConstant.java
@@ -0,0 +1,15 @@
+package com.imooc.constant;
+
+/**
+ * @program: sell
+ * @Date: 2018/9/28 19:21
+ * @Author: Mr.lv
+ * @Description: redis常量
+ */
+public interface RedisConstant {
+
+    String TOKEN_PREFER = "token_%s";
+
+    Integer EXPIRE = 7200; // 2小时
+
+}
