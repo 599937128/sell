9fd20a6699831ce845053be249bd8b16ef5782d8
lvliao <git@code.aliyun.com:te_lv02/test2.git>
2018-10-03 16:57:17 +0800

    秒杀代码的书写需要使用redis作为分布式锁

--- src/main/java/com/imooc/SellApplication.java
+++ src/main/java/com/imooc/SellApplication.java
@@ -4,6 +4,10 @@ import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * 引入websocket之后必须给每一个测试类加上 类似下边的注解 引入web环境,否则就不能加载到websocke的bean
+ * @SpringBootTest(classes = SellApplication.class, webEnvironment = SpringBootTest.WebEnvironment.DEFINED_PORT)
+ */
 @SpringBootApplication
 @MapperScan(basePackages = "com.imooc.dataobject.mapper")
 public class SellApplication {
